0b18ffc2c4c90169c47bdbe0d5c81f295bd14574
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-09-05 05:35:59 -0300

    config basic inicial, install do browser-sync e css

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+(function(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCLHWRMHNRi-B7r0uXockZkHEJ0z1gwZKU",
+    authDomain: "valetransporte-72413.firebaseapp.com",
+    databaseURL: "https://valetransporte-72413.firebaseio.com",
+    projectId: "valetransporte-72413",
+    storageBucket: "valetransporte-72413.appspot.com",
+    messagingSenderId: "3535751972",
+    appId: "1:3535751972:web:26d0bc1341e1fc8f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+})()
